758233659ebf11d16cbc4d1e96b11422193a4620
Grant Holley <gvholley@gmail.com>
2021-01-02 17:58:45 +0900

    Refactored events

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-const database = [
+const button = document.getElementById("enter");
+const input = document.getElementById("userinput");
+const ul = document.querySelector("ul");
+
+function inputLength() {
+  return input.value.length;
+}
+
+function createListElement() {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+}
+
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+    }
+}
+
+
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+/*const database = [
 
 {
   username: "grant",
@@ -44,4 +76,4 @@ for (var i=0; i < database.length; i++) {
 const userNamePrompt = prompt("What's your username?");
 const passwordPrompt = prompt("What's your password?");
 
-signIn(userNamePrompt, passwordPrompt);
+signIn(userNamePrompt, passwordPrompt); */
