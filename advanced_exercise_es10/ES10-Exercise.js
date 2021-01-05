a8998e0ffdacc7f7d07902525f15e21df329a9fe
Grant Holley <gvholley@gmail.com>
2021-01-05 11:44:19 +0900

    Finished exercise 10

--- advanced_exercise_es10/ES10-Exercise.js
+++ advanced_exercise_es10/ES10-Exercise.js
@@ -27,6 +27,7 @@ const users = { user1: 18273, user2: 92833, user3: 90315 }
 
 
 //#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
-
+mutiplyArray = usersArray.map((user) => [user[0], user[1] * 2])
 
 //#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
+const updatedUsers = Object.fromEntries(multiplyArray)
